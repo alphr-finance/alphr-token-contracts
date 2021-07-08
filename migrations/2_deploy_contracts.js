e55fc6db48c891360bd90718fa4eba22f3625f3a
smart-ace <smart.solidity@gmail.com>
2021-07-08 05:17:14 -0400

    Update deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,49 +1,42 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const ALPHRToken = artifacts.require("ALPHRToken");
-const TokenVault = artifacts.require("TokenVault");
-const TeamVesting = artifacts.require("TokenVesting");
-const ProtocolRewardsVesting = artifacts.require("TokenVesting");
-const LPRewardsVesting = artifacts.require("TokenVesting");
-const CDFVesting = artifacts.require("TokenVesting");
+const ALPHRToken = artifacts.require('ALPHRToken')
+const TokenVault = artifacts.require('TokenVault')
+const TeamVesting = artifacts.require('TokenVesting')
+const ProtocolRewardsVesting = artifacts.require('TokenVesting')
+const LPRewardsVesting = artifacts.require('TokenVesting')
+const CDFVesting = artifacts.require('TokenVesting')
 
 module.exports = async function (deployer) {
-  await deployer.deploy(ALPHRToken);
-  await deployer.deploy(TokenVault);
+  await deployer.deploy(ALPHRToken)
+  await deployer.deploy(TokenVault)
   // Team Vesting
   await deployer.deploy(
     TeamVesting,
-    "0x793565878C643d507EC3365229Da5495c2F76A29",
+    process.env.KOVAN_BENEFICIARY || '0x793565878C643d507EC3365229Da5495c2F76A29',
     1624345200,
     2592000,
     23,
     ALPHRToken.address
-  );
+  )
   // Protocol Rewards Vesting
   await deployer.deploy(
     ProtocolRewardsVesting,
-    "0x77E36C96166177337f54352837e7bBce6C437d44",
+    process.env.KOVAN_BENEFICIARY || '0x77E36C96166177337f54352837e7bBce6C437d44',
     1621468800,
     86400,
     1,
     ALPHRToken.address
-  );
+  )
   // LP Rewards Vesting
   await deployer.deploy(
     LPRewardsVesting,
-    "0x77E36C96166177337f54352837e7bBce6C437d44",
+    process.env.KOVAN_BENEFICIARY || '0x77E36C96166177337f54352837e7bBce6C437d44',
     1620370800,
     86400,
     1,
     ALPHRToken.address
-  );
+  )
   // CDF Vesting
-  await deployer.deploy(
-    CDFVesting,
-    TokenVault.address,
-    1624345200,
-    2592000,
-    48,
-    ALPHRToken.address
-  );
-};
+  await deployer.deploy(CDFVesting, TokenVault.address, 1624345200, 2592000, 48, ALPHRToken.address)
+}
